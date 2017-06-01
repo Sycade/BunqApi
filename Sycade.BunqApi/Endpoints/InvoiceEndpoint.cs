758732ab25d49edd2b8d9fa9ec3b622095c876bd
Michiel Lowijs <mlowijs@gmail.com>
2017-06-01 11:10:56 +0200

    Added GetPdfContentAsync call

--- Sycade.BunqApi/Endpoints/InvoiceEndpoint.cs
+++ Sycade.BunqApi/Endpoints/InvoiceEndpoint.cs
@@ -1,5 +1,6 @@
 ﻿using Sycade.BunqApi.Model.Invoices;
 using Sycade.BunqApi.Responses;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -19,6 +20,11 @@ namespace Sycade.BunqApi.Endpoints
             var entities = await ApiClient.DoSignedApiRequestAsync(HttpMethod.Get, $"user/{session.User.Id}/invoice", session.Token);
 
             return entities.Cast<Invoice>().ToArray();
+        }
+
+        public async Task<Stream> GetPdfContentAsync(long invoiceId, Session session)
+        {
+            return await ApiClient.DoRawApiRequestAsync(HttpMethod.Get, $"user/{session.User.Id}/invoice/{invoiceId}/pdf-content", session.Token);
         }
     }
 }
